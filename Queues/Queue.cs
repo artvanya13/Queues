b7ae22ec79d0817aee21aad1c67bd0b37ae7eedd
Vanya Artiukhov <K038486@eltham-college.org.uk>
2021-03-25 12:47:27 +0000

    Add project files.

---
+++ Queues/Queue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Queues
+{
+    class Queue
+    {
+        public int front = 0;
+        public int end = 0;
+        private int[] array;
+
+        public Queue (int size)
+        {
+            array = new int[5];
+        }
+
+
+        public void AddToQueue(int item)
+        {
+            array[end] = item;
+            end = (end + 1) % array.Length;
+        }
+
+        public int RemoveFromQueue(int item)
+        {
+            int k;
+            for (int i = 0; i < array.Length; i ++)
+            {
+                if (i == item)
+                    k = i; 
+            }
+            return array[k];
+            front++;
+        }
+    }
+}
