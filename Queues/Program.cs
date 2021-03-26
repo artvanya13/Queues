65dbede91b4e105bccf6c8342d397384e8595b6f
Vanya Artiukhov <K038486@eltham-college.org.uk>
2021-03-26 11:34:03 +0000

    Queues project finished

--- Queues/Program.cs
+++ Queues/Program.cs
@@ -7,7 +7,12 @@ namespace Queues
         static void Main(string[] args)
         {
             Queue s = new Queue(5);
+            s.AddToQueue("Hashir");
+            s.AddToQueue("Michael");
+            s.AddToQueue("Inna");
             s.AddToQueue("Vanya");
+            s.AddToQueue("Sasha");
+            s.RemoveFromQueue();
             s.Print();
         }
     }
